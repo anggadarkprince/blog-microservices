8859910a309acb62f257e486aa210d6409fa59b5
Angga Ari Wijaya <anggadarkprince@gmail.com>
2022-08-25 11:54:30 +0700

    feat(moderation): add moderation service and restore event

--- comments/index.js
+++ comments/index.js
@@ -23,20 +23,39 @@ app.post('/posts/:id/comments', async (req, res) => {
 
     const comments = commentsByPostId[postId] || [];
 
-    comments.push({id: commentId, content});
+    comments.push({id: commentId, content, status: 'pending'});
 
     commentsByPostId[postId] = comments;
 
     await axios.post('http://localhost:4005/events', {
         type: 'CommentCreated',
-        data: {id: commentId, content, postId}
+        data: {id: commentId, content, postId, status: 'pending'}
     });
 
     res.status(201).send(comments);
 });
 
-app.post('/events', (req, res) => {
-    console.log(req.body);
+app.post('/events', async (req, res) => {
+    console.log('Event Received:', req.body);
+
+    const {type, data} = req.body;
+
+    if (type === 'CommentModerated') {
+        const {postId, id, status, content} = data;
+
+        const comments = commentsByPostId[postId];
+
+        const comment = comments.find(comment => {
+            return comment.id === id;
+        });
+        comment.status = status;
+
+        await axios.post('http://localhost:4005/events', {
+            type: 'CommentUpdated',
+            data: {id: id, content, postId, status}
+        });
+    }
+
     res.send({});
 });
 
